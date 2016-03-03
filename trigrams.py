4d1f34508d60107c6555972786b6c20e2d4dbd41
Hannah Krager <hannahkrager@gmail.com>
2016-03-03 13:55:42 -0800

    some changes to output

--- trigrams.py
+++ trigrams.py
@@ -8,14 +8,12 @@ def list_words(word_list):
     for n in range(0, len(word_list) - 2):
         key = tuple(word_list[n:n + 2])
         value = word_list[n + 2]
-        if key in dictionary:
-            dictionary[key].append(value)
-        else:
-            dictionary[key] = [value]
+        dictionary.setdefault(key, []).append(value)
     return dictionary
 
 
 def remove_punctuation(list_of_words):
+    list_of_words = list_of_words.lower()
     return ''.join([c for c in list_of_words if c not in (':', ']',
     '[', '#', '*', '(', ')', '-', '!', '?', ',', "'", '"', '.')])
 
@@ -25,16 +23,15 @@ def create_text(dictionary, wordcount=50):
     output = [choice[0], choice[1]]
     output.append(dictionary[choice][0])
 
-    while len(output) < wordcount + 1:
+    while len(output) < wordcount:
         output = make_sentence(dictionary, output)
-        print('Sentence created')
-    output = ' '.join(output)
-    print(output)
+    output = output[:wordcount]
+    return ' '.join(output)
 
 
 def make_sentence(dictionary, output):
     search_word = output[-1]
-    for n in range(random.randint(2, 4)):
+    for n in range(random.randint(1, 3)):
         for key in dictionary:
             if key[0] == search_word:
                 output.append(key[1])
@@ -43,16 +40,23 @@ def make_sentence(dictionary, output):
     return output
 
 
+def write_to_file(output):
+    outfile = io.open('output.txt', 'w')
+    outfile.write(output)
+    outfile.close()
+
+
 def main(path, word_count):
-    infile = io.open(path, 'r')
+    infile = io.open(path, encoding='utf-8')
     text_string = infile.read()
     infile.close()
+    # execute funtions
     clean_text_string = remove_punctuation(text_string)
     list_of_strings = clean_text_string.split()
     dictionary = list_words(list_of_strings)
-    create_text(dictionary)
-
+    output = create_text(dictionary, word_count)
+    write_to_file(output)
 
 if __name__ == '__main__':
-    #print(str(sys.argv))
-    main(sys.argv[1], sys.argv[2])
+    word_count = int(sys.argv[2])
+    main(sys.argv[1], word_count)
