3e8b8be4bafc02692c2b630fb6f7c31f9a02941b
Hannah Krager <hannahkrager@gmail.com>
2016-03-02 16:08:13 -0800

    add test and function to remove punctuation from string

--- test_trigrams.py
+++ test_trigrams.py
@@ -10,7 +10,16 @@ TEST_OUTPUT = {('on', 'the'): ['twentieth'], ('night--it', 'was'): ['on'],
                ('the', 'twentieth'): ['of'],
                ('March', 'One'): ['night--it']}
 
+TEST_STRING = 'The - dog ! ran (away) [from] ?'
+
+STRING_OUTPUT = 'The  dog  ran away from '
+
 
 def test_list_words():
     from trigrams import list_words
     assert list_words(TEST_LIST) == TEST_OUTPUT
+
+
+def test_remove_punctuation():
+    from trigrams import remove_punctuation
+    assert remove_punctuation(TEST_STRING) == STRING_OUTPUT
--- trigrams.py
+++ trigrams.py
@@ -1,9 +1,8 @@
 import io
 
-dictionary = {}
-
 
 def list_words(word_list):
+    dictionary = {}
     for n in range(0, len(word_list) - 2):
         key = word_list[n], word_list[n + 1]
         value = word_list[n + 2]
@@ -12,13 +11,26 @@ def list_words(word_list):
         else:
             value = [value]
             dictionary.update({key: value})
+    return dictionary
+
+
+def remove_punctuation(list_of_words):
+    return ''.join([c for c in list_of_words if c not in (':', ']', '[', '#', '*', '(', ')',  '-', '!', '?', ',', "'", '"', '.')])
+
+
+def create_text(dictionary):
+    pass
 
 
 def main(path, word_count):
     infile = io.open(path, 'r')
-    list_of_strings = infile.read().split()
+    text_string = infile.read()
+    text_string = remove_punctuation(text_string)
+    print(text_string)
+    list_of_strings = text_string.split()
     infile.close()
-    list_words(list_of_strings)
+    dictionary = list_words(list_of_strings)
+    create_text(dictionary)
 
 
 if __name__ == '__main__':
